df0f508ce91dcaf2f5560ae3a8d7f5fd2c64089b
grolland <ilya.play111@gmail.com>
2023-07-23 19:24:13 +0300

    1.00

--- player.js
+++ player.js
@@ -5,98 +5,98 @@ const MOVE_RIGHT_KEYS_CODES = ["ArrowRight", "KeyD"];
 const ALL_MOVE_KEY_CODES = [...MOVE_UP_KEY_CODES, ...MOVE_DOWN_KEYS_CODES, ...MOVE_LEFT_KEYS_CODES, ...MOVE_RIGHT_KEYS_CODES];
 
 export class Player {
-  constructor(x, y, context, movementLimits) {
-    this.radius = 15;
-    this.velocity = 3;
+   constructor(x, y, context, movementLimits) {
+      this.radius = 15;
+      this.velocity = 3;
 
-    this.x = x;
-    this.y = y;
-    this.context = context;
-    this.cursorPosition = {
-      x: 0,
-      y: 0
-    };
-    this.movementLimits = {
-      minX: movementLimits.minX + this.radius,
-      maxX: movementLimits.maxX - this.radius,
-      minY: movementLimits.minY + this.radius,
-      maxY: movementLimits.maxY - this.radius,
-    };
+      this.x = x;
+      this.y = y;
+      this.context = context;
+      this.cursorPosition = {
+         x: 0,
+         y: 0
+      };
+      this.movementLimits = {
+         minX: movementLimits.minX + this.radius,
+         maxX: movementLimits.maxX - this.radius,
+         minY: movementLimits.minY + this.radius,
+         maxY: movementLimits.maxY - this.radius,
+      };
 
-    this.keyMap = new Map();
-    document.addEventListener("mousemove", (event => {
-      this.cursorPosition.x = event.clientX;
-      this.cursorPosition.y = event.clientY;
-    }));
-    document.addEventListener("keydown", (event) => this.keyMap.set(event.code, true));
-    document.addEventListener("keyup", (event) => this.keyMap.delete(event.code));
+      this.keyMap = new Map();
+      document.addEventListener("mousemove", (event => {
+         this.cursorPosition.x = event.clientX;
+         this.cursorPosition.y = event.clientY;
+      }));
+      document.addEventListener("keydown", (event) => this.keyMap.set(event.code, true));
+      document.addEventListener("keyup", (event) => this.keyMap.delete(event.code));
 
-    this.image = new Image();
-    this.image.src = "./img/player.png";
-    this.imageWidth = 50;
-    this.imageHeight = 60;
-    this.isMoving = false;
-    this.imageTick = 0;
-  }
-
-  drawImg() {
-    const imageTickLimit = 18;
-    let subX = 0;
-    if (!this.isMoving) {
-      subX = 0;
+      this.image = new Image();
+      this.image.src = "./img/player.png";
+      this.imageWidth = 50;
+      this.imageHeight = 60;
+      this.isMoving = false;
       this.imageTick = 0;
-    } else {
-      subX = this.imageTick > imageTickLimit ? this.imageWidth * 2 : this.imageWidth;
-      this.imageTick++;
-    }
-    if (this.imageTick > imageTickLimit * 2) {
-      this.imageTick = 0;
-    }
+   }
 
-    this.context.drawImage(
-      this.image,
-      subX,
-      0,
-      this.imageWidth,
-      this.imageHeight,
-      this.x - this.imageWidth / 2,
-      this.y - this.imageHeight / 2,
-      this.imageWidth,
-      this.imageHeight);
-  }
+   drawImg() {
+      const imageTickLimit = 18;
+      let subX = 0;
+      if (!this.isMoving) {
+         subX = 0;
+         this.imageTick = 0;
+      } else {
+         subX = this.imageTick > imageTickLimit ? this.imageWidth * 2 : this.imageWidth;
+         this.imageTick++;
+      }
+      if (this.imageTick > imageTickLimit * 2) {
+         this.imageTick = 0;
+      }
 
-  draw() {
-    this.context.save();
-    let angle = Math.atan2(this.cursorPosition.y - this.y, this.cursorPosition.x - this.x);
-    this.context.translate(this.x, this.y);
-    this.context.rotate(angle + Math.PI / 2);
-    this.context.translate(-this.x, -this.y);
-    this.drawImg();
-    this.context.restore();
-  }
+      this.context.drawImage(
+         this.image,
+         subX,
+         0,
+         this.imageWidth,
+         this.imageHeight,
+         this.x - this.imageWidth / 2,
+         this.y - this.imageHeight / 2,
+         this.imageWidth,
+         this.imageHeight);
+   }
 
-  update() {
-    this.draw();
-    this.isMoving = this.shouldMove(ALL_MOVE_KEY_CODES);
-    this.updatePosition();
-    this.checkPositionLimitAndUpdate();
-  }
+   draw() {
+      this.context.save();
+      let angle = Math.atan2(this.cursorPosition.y - this.y, this.cursorPosition.x - this.x);
+      this.context.translate(this.x, this.y);
+      this.context.rotate(angle + Math.PI / 2);
+      this.context.translate(-this.x, -this.y);
+      this.drawImg();
+      this.context.restore();
+   }
 
-  updatePosition() {
-    if (this.shouldMove(MOVE_UP_KEY_CODES)) this.y -= this.velocity;
-    if (this.shouldMove(MOVE_DOWN_KEYS_CODES)) this.y += this.velocity;
-    if (this.shouldMove(MOVE_LEFT_KEYS_CODES)) this.x -= this.velocity;
-    if (this.shouldMove(MOVE_RIGHT_KEYS_CODES)) this.x += this.velocity;
-  }
+   update() {
+      this.draw();
+      this.isMoving = this.shouldMove(ALL_MOVE_KEY_CODES);
+      this.updatePosition();
+      this.checkPositionLimitAndUpdate();
+   }
 
-  checkPositionLimitAndUpdate() {
-    if (this.y < this.movementLimits.minY) this.y = this.movementLimits.minY;
-    if (this.y > this.movementLimits.maxY) this.y = this.movementLimits.maxY;
-    if (this.x < this.movementLimits.minX) this.x = this.movementLimits.minX;
-    if (this.x > this.movementLimits.maxX) this.x = this.movementLimits.maxX;
-  }
+   updatePosition() {
+      if (this.shouldMove(MOVE_UP_KEY_CODES)) this.y -= this.velocity;
+      if (this.shouldMove(MOVE_DOWN_KEYS_CODES)) this.y += this.velocity;
+      if (this.shouldMove(MOVE_LEFT_KEYS_CODES)) this.x -= this.velocity;
+      if (this.shouldMove(MOVE_RIGHT_KEYS_CODES)) this.x += this.velocity;
+   }
 
-  shouldMove(keys) {
-    return keys.some(key => this.keyMap.get(key));
-  }
+   checkPositionLimitAndUpdate() {
+      if (this.y < this.movementLimits.minY) this.y = this.movementLimits.minY;
+      if (this.y > this.movementLimits.maxY) this.y = this.movementLimits.maxY;
+      if (this.x < this.movementLimits.minX) this.x = this.movementLimits.minX;
+      if (this.x > this.movementLimits.maxX) this.x = this.movementLimits.maxX;
+   }
+
+   shouldMove(keys) {
+      return keys.some(key => this.keyMap.get(key));
+   }
 }
--- script.js
+++ script.js
@@ -20,7 +20,7 @@ let particles = [];
 let score = 0;
 let best = 0;
 if (localStorage.getItem('best')) {
-  best = +localStorage.getItem('best')
+   best = +localStorage.getItem('best')
 }
 bestEl.innerHTML = best
 let animationId;
@@ -28,133 +28,133 @@ let countIntervalId;
 let spawnIntervalId;
 
 playAgainEl.addEventListener('click', playAgain)
-document.addEventListener('keyup', event => { 
-  if((event.code === 'Enter' || 'Space') && wastedElement.style.display == "flex" ) {
-    playAgain()
-  }
+document.addEventListener('keyup', event => {
+   if ((event.code === 'Enter' || 'Space') && wastedElement.style.display == "flex") {
+      playAgain()
+   }
 })
 
 startGame();
 
 function startGame() {
-  init();
-  animate();
-  spawnEnemies();
+   init();
+   animate();
+   spawnEnemies();
 }
 
 
 function init() {
-  const movementLimits = {
-    minX: 0,
-    maxX: canvas.width,
-    minY: 0,
-    maxY: canvas.height,
-  };
-  player = new Player(canvas.width / 2, canvas.height / 2, context, movementLimits);
-  addEventListener("click", createProjectile);
+   const movementLimits = {
+      minX: 0,
+      maxX: canvas.width,
+      minY: 0,
+      maxY: canvas.height,
+   };
+   player = new Player(canvas.width / 2, canvas.height / 2, context, movementLimits);
+   addEventListener("click", createProjectile);
 }
 
 function createProjectile(event) {
-  projectiles.push(
-    new Projectile(
-      player.x,
-      player.y,
-      event.clientX,
-      event.clientY,
-      context
-    )
-  );
+   projectiles.push(
+      new Projectile(
+         player.x,
+         player.y,
+         event.clientX,
+         event.clientY,
+         context
+      )
+   );
 };
 
 function spawnEnemies() {
-  let countOfSpawnEnemies = 1;
+   let countOfSpawnEnemies = 1;
 
-  countIntervalId = setInterval(() => countOfSpawnEnemies++, 30000);
-  spawnIntervalId = setInterval(() => spawnCountEnemies(countOfSpawnEnemies), 1000);
+   countIntervalId = setInterval(() => countOfSpawnEnemies++, 30000);
+   spawnIntervalId = setInterval(() => spawnCountEnemies(countOfSpawnEnemies), 1000);
 
-  spawnCountEnemies(countOfSpawnEnemies)
+   spawnCountEnemies(countOfSpawnEnemies)
 }
 
 function spawnCountEnemies(count) {
-  for (let i = 0; i < count; i++) {
-    enemies.push(new Enemy(canvas.width, canvas.height, context, player));
-  }
+   for (let i = 0; i < count; i++) {
+      enemies.push(new Enemy(canvas.width, canvas.height, context, player));
+   }
 }
 
 function animate() {
-  animationId = requestAnimationFrame(animate);
-  context.clearRect(0, 0, canvas.width, canvas.height);
+   animationId = requestAnimationFrame(animate);
+   context.clearRect(0, 0, canvas.width, canvas.height);
 
-  particles = particles.filter(particle => particle.alpha > 0);
-  projectiles = projectiles.filter(projectileInsideWindow);
-  enemies.forEach(enemy => checkHittingEnemy(enemy));
-  enemies = enemies.filter(enemy => enemy.health > 0);
-  const isGameOver = enemies.some(checkHittingPlayer);
-  if (isGameOver) {
-    wastedElement.style.display = "flex";
-    clearInterval(countIntervalId);
-    clearInterval(spawnIntervalId);
-    cancelAnimationFrame(animationId);
-  }
+   particles = particles.filter(particle => particle.alpha > 0);
+   projectiles = projectiles.filter(projectileInsideWindow);
+   enemies.forEach(enemy => checkHittingEnemy(enemy));
+   enemies = enemies.filter(enemy => enemy.health > 0);
+   const isGameOver = enemies.some(checkHittingPlayer);
+   if (isGameOver) {
+      wastedElement.style.display = "flex";
+      clearInterval(countIntervalId);
+      clearInterval(spawnIntervalId);
+      cancelAnimationFrame(animationId);
+   }
 
-  particles.forEach(particle => particle.update());
-  projectiles.forEach(projectile => projectile.update());
-  player.update();
-  enemies.forEach(enemy => enemy.update());
+   particles.forEach(particle => particle.update());
+   projectiles.forEach(projectile => projectile.update());
+   player.update();
+   enemies.forEach(enemy => enemy.update());
 }
 
 function projectileInsideWindow(projectile) {
-  return projectile.x + projectile.radius > 0 &&
-    projectile.x - projectile.radius < canvas.width &&
-    projectile.y + projectile.radius > 0 &&
-    projectile.y - projectile.radius < canvas.height
+   return projectile.x + projectile.radius > 0 &&
+      projectile.x - projectile.radius < canvas.width &&
+      projectile.y + projectile.radius > 0 &&
+      projectile.y - projectile.radius < canvas.height
 }
 
 function checkHittingPlayer(enemy) {
-  const distance = distanceBetweenTwoPoints(player.x, player.y, enemy.x, enemy.y);
-  return distance - enemy.radius - player.radius < 0;
+   const distance = distanceBetweenTwoPoints(player.x, player.y, enemy.x, enemy.y);
+   return distance - enemy.radius - player.radius < 0;
 }
 
 function checkHittingEnemy(enemy) {
-  projectiles.some((projectile, index) => {
-    const distance = distanceBetweenTwoPoints(projectile.x, projectile.y, enemy.x, enemy.y);
-    if (distance - enemy.radius - projectile.radius > 0) return false;
+   projectiles.some((projectile, index) => {
+      const distance = distanceBetweenTwoPoints(projectile.x, projectile.y, enemy.x, enemy.y);
+      if (distance - enemy.radius - projectile.radius > 0) return false;
 
-    removeProjectileByIndex(index);
-    enemy.health--;
+      removeProjectileByIndex(index);
+      enemy.health--;
 
-    if (enemy.health < 1) {
-      increaseScore();
-      updateBest()
-      enemy.createExplosion(particles);
-    }
+      if (enemy.health < 1) {
+         increaseScore();
+         updateBest()
+         enemy.createExplosion(particles);
+      }
 
-    return true;
-  });
+      return true;
+   });
 }
 
 function removeProjectileByIndex(index) {
-  projectiles.splice(index, 1);
+   projectiles.splice(index, 1);
 }
 
 function increaseScore() {
-  score += 250;
-  scoreEl.innerHTML = score;
+   score += 250;
+   scoreEl.innerHTML = score;
 }
 
 function updateBest() {
-  if (+score > +best){
-    localStorage.setItem('best', score.toString())
-    bestEl.innerHTML = score
-  }
+   if (+score > +best) {
+      localStorage.setItem('best', score.toString())
+      bestEl.innerHTML = score
+   }
 }
 
 function playAgain() {
-  projectiles = [];
-  enemies = [];
-  particles = [];
-  wastedElement.style.display = "none";
-  score = 0
-  scoreEl.innerHTML = score
-  startGame()
+   projectiles = [];
+   enemies = [];
+   particles = [];
+   wastedElement.style.display = "none";
+   score = 0
+   scoreEl.innerHTML = score
+   startGame()
 }
